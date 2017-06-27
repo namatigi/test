745c3023c043c3b09ea29120f13530893d1ec7bb
namatigi <leon.mangu@gmail.com>
2017-06-27 21:31:12 +0300

    refactored add_new_subject version

--- new_subject.php
+++ new_subject.php
@@ -1,68 +1,14 @@
 <?php include("includes/connection.php");?>
 <?php include("includes/header.php"); ?>
-
 <?php require_once("includes/functions.php");?>
-<?php
-if(isset($_GET['subj'])){
-    $sel_subj =$_GET['subj'];
-    $sel_subject= get_subject_by_id($sel_subj);
-    $sel_pages=NULL;
-    $sel_pg=0;
-}elseif(isset($_GET['page'])){
-    $sel_subj=0;
-    $sel_subject=NULL;
-    $sel_pages = $_GET['page'];
-    $sel_pages = get_page_by_id($sel_pages);
-}else{
-    $sel_pages=NULL;
-    $sel_subject=NULL;
-    $sel_pg=0;
-    $sel_subj=0;
-}
-
-?>
+<?php find_selected_page();?>
 
 <table id="structure">
     <tr>
         <td id="navigation">
-            <ul class="subjects">
-                <?php
-
-                #subject database query
-                $subject_set=get_all_subjects();
-
-                #use returned subjects
-                while($subject=mysqli_fetch_array($subject_set)) {
-                    echo "<li ";
-                    if($subject["id"]==$sel_subj) {
-                        echo "class=\"selected\"";
-                    }
-                    echo "><a href=\"content.php?subj=" .urlencode($subject["id"])."\">
-                 {$subject['menu_name']}
-                </a></li>";
-
-                    #pages database query.
-                    $page_set=get_pages_for_subject($subject["id"]);
-
-                    #use returned pages
-                    echo "<ul class=\"pages\">";
-                    while ($page = mysqli_fetch_array($page_set)) {
-                        echo "<li ";
-                        if($page["id"]==$sel_pages ) {
-                            echo "class=\"selected\"";
-                        }
-                        echo "><a href=\"content.php?page=" .urlencode($page["id"])."\">
-                    {$page['menu_name']}
-                    </a></li>";
-                    }
-
-                    echo "</ul>";
-
-                }
-
-                ?>
-            </ul>
-
+            <?php echo navigation($sel_subject,$sel_page); ?>
+            <br/>
+            <a href="new_subject.php ">+ Add a new subject</a>
         </td>
         <td id="page">
 
