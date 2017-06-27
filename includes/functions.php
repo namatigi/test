494faade18494da106eeb4e445f564f34691ddc6
namatigi <leon.mangu@gmail.com>
2017-06-27 21:33:26 +0300

    refactoring function version

--- includes/functions.php
+++ includes/functions.php
@@ -39,7 +39,6 @@
         }
     }
 
-
     function get_all_subjects(){
         global $link;
         $query = "SELECT *  FROM subject ORDER BY position ASC";
@@ -50,7 +49,6 @@
         return $subject_set;
     }
 
-
     function get_pages_for_subject($subject_id){
         global $link;
         $query="SELECT * FROM pages WHERE subject_id={$subject_id} ORDER BY position ASC";
@@ -59,7 +57,6 @@
         confirm_query($page_set);
         return $page_set;
     }
-
 
     function get_subject_by_id($subject_id){
         global $link;
@@ -96,25 +93,50 @@
         }
     }
 
-
     function find_selected_page(){
         global $sel_subject;
-        global $sel_pages;
+        global $sel_page;
         if(isset($_GET['subj'])){
-            $sel_subj =$_GET['subj'];
-            $sel_subject= get_subject_by_id($sel_subj);
-            $sel_pages=NULL;
-            $sel_pg=0;
+            $sel_subject= get_subject_by_id($_GET['subj']);
+            $sel_page=NULL;
         }elseif(isset($_GET['page'])){
-            $sel_subj=0;
             $sel_subject=NULL;
-            $sel_pages = $_GET['page'];
-            $sel_pages = get_page_by_id($sel_pages);
+            $sel_page = get_page_by_id($_GET['page']);
         }else{
-            $sel_pages=NULL;
+            $sel_page=NULL;
             $sel_subject=NULL;
-            $sel_pg=0;
-            $sel_subj=0;
         }
     }
+
+    function navigation($sel_subject,$sel_page){
+        $output = "<ul class=\"subjects\">";
+
+
+
+                $subject_set=get_all_subjects();
+                while($subject=mysqli_fetch_array($subject_set)) {
+                    $output .= "<li ";
+                    if($subject["id"]==$sel_subject["id"]) {$output .= "class=\"selected\"";}
+                    $output .= "><a href=\"edit_subject.php?subj=" .urlencode($subject["id"])."\">{$subject['menu_name']}</a></li>";
+
+
+
+                    $page_set=get_pages_for_subject($subject["id"]);
+                    $output .="<ul class=\"pages\">";
+
+                    while ($page = mysqli_fetch_array($page_set)) {
+                        $output .= "<li ";
+                        if($page["id"]==$sel_page['id']) {$output .= "class=\"selected\"";}
+                        $output .= "><a href=\"content.php?page=" .urlencode($page["id"])."\">{$page['menu_name']}</a></li>";
+                    }
+                    $output .= "</ul>";
+                }
+
+
+            $output .= "</ul>";
+
+
+        return $output;
+    }
+
 ?>
